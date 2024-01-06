a28392caed6a934c963d50d87d1b963c98dfcba4
Tushar Gohil <tushar.gohil@scet.ac.in>
2024-01-06 23:43:53 +0530

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>iHundred</span>,
+  footer: {
+    text: 'All Rights Reserved, iHundred Development Team.',
+  },
+}
+
+export default config
